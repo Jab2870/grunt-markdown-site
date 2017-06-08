718c54c17cead79eb0eb5dd25786acfac1b1606e
Jonathan Hodgson <jonathan@lunarweb.co.uk>
2017-06-08 19:09:11 +0100

    Tidy and remove connect

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,62 +1,51 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    
-    site: {
-      site: {
-        options: {
-          site: {
-            title: 'Example',
-          },
-          templates: 'site/templates'
-        },
-        src: 'site/content',
-        dest: 'dest'
-      }
-    },
-    
-    connect: {
-      site: {
-        options: {
-          port: 8000,
-          base: 'dest',
-          livereload: true          
-        }
-      }
-    },
-    
-    jshint: {
-      task: {
-        src: ['tasks/site.js']
-      }
-    },
+	grunt.initConfig({
+		
+		site: {
+			site: {
+				options: {
+					site: {
+						title: 'Example',
+					},
+					templates: 'site/templates'
+				},
+				src: 'site/content',
+				dest: 'dest'
+			}
+		},
+		
+		jshint: {
+			task: {
+				src: ['tasks/site.js']
+			}
+		},
 
-    watch: {
-      reload: {
-        files: [
-          'dest/**/*'
-        ],
-        options: {
-          livereload: true
-        }
-      },
-      site: {
-        files: ['site/**/*'],
-        tasks: ['site']
-      },
-      task: {
-        files: ['tasks/*.js']
-      }
-    }
-    
-  });
+		watch: {
+			reload: {
+				files: [
+					'dest/**/*'
+				],
+				options: {
+					livereload: true
+				}
+			},
+			site: {
+				files: ['site/**/*'],
+				tasks: ['site']
+			},
+			task: {
+				files: ['tasks/*.js']
+			}
+		}
+		
+	});
 
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');  
-  grunt.loadTasks('tasks');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');	
+	grunt.loadTasks('tasks');
 
-  grunt.registerTask('build', ['site']);
-  grunt.registerTask('default', ['jshint', 'site', 'connect', 'watch']);
+	grunt.registerTask('build', ['site']);
+	grunt.registerTask('default', ['jshint', 'site', 'watch']);
 
 };
